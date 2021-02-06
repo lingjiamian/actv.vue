0ce6189d20e67cbd5bf4f862d3073b87c2cbdc2c
lingjiamian <836376242@qq.com>
2021-02-06 23:50:56 +0800

    上传到github

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-row id="app">
+    <el-col :span="10" :offset="7">
+      <el-row class="wrapper">
+        <el-row class="header-logo">
+          <el-col :span="5" :offset="1">
+            <div class="logo">
+             
+            </div>
+          </el-col>
+        </el-row>
+        <el-row class="header-tv">
+          <el-col :span=4 :offset="3">
+            <el-image :src="logoUrl" :fit="contain">
+
+            </el-image>
+          </el-col>
+          <el-col :span="4" :offset="8">
+            <h1>看电视</h1>
+            <h4>just for fun</h4>
+          </el-col>
+        </el-row>
+        <br>
+        <br>
+        <el-row class="nav">
+          <el-col>
+            <router-link to="/">
+              <el-link> 首页 </el-link>
+            </router-link>
+            |
+            <router-link to="/about">
+              <el-link>关于 </el-link>
+            </router-link>
+          </el-col>
+        </el-row>
+
+        <br>
+        <router-view />
+        <el-row class="footer">
+          power by .net5.0
+        </el-row>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +51,39 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 3%;
 }
+
+.nav a {
+  text-decoration: none;
+}
+
+.wrapper {
+  background-color: white;
+  padding-top: 80px;
+}
+
+/* #nav {
+  padding: 30px;
+} */
+
+/* #nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
 </style>
+
+<script>
+export default {
+  data(){
+    return{
+      fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
+      logoUrl:require('./assets/logo.jpg')
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-row class="home">
+    <el-row class="tv-title">
+      <el-col :span="5" :offset="1">
+        <h2>鬼灭之刃21</h2>
+      </el-col>
+    </el-row>
+    <br />
+    <el-row class="tv-info">
+      <el-col :span="3" :offset="1"
+        ><i class="el-icon-date"></i>2021/1/15</el-col
+      >
+      <el-col :span="2"><i class="el-icon-chat-dot-round"></i>17</el-col>
+      <el-col :span="2"><i class="el-icon-view"></i>123</el-col>
+    </el-row>
+    <br />
+    <br />
+
+    <div id="dplayer1"></div>
+
+<br>
+<br>
+    <el-row class="tv-comment">
+        <comment-editor @submit="submitComment"></comment-editor>
+
+    </el-row>
+  </el-row>
+</template>
+
+<style>
+.tv-info {
+  font-size: 0.8em;
+  color: #606266;
+}
+</style>
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import DPlayer from "dplayer";
+import Vue from 'vue'
+import CommentEditor from 'comment-message-editor'
+
+Vue.component(CommentEditor.name, CommentEditor)
+
+export default {
+  name: "Home",
+  components: {},
+  data(){
+    return{
+      textarea:"",
+      input:"",
+    }
+  },
+  mounted() {
+    const dp = new DPlayer({
+      container: document.getElementById("dplayer1"),
+      
+      video: {
+        url: require("/12.mp4"),
+        
+      },
+    });
+    console.log(dp);
+  },
+
+  methods:{
+    submitComment(comment){
+      console.log(comment);
+    }
+  }
+};
+</script>
+
